234b369d21ef5c36eec26874ad75cfa5de8ddfda
Nguyễn Hữu Tài <vnnguyenhuutai2000@gmail.com>
2022-11-10 01:01:58 +0700

    feat: deploy

--- src/App.js
+++ src/App.js
@@ -1,33 +1,75 @@
-import React, { useState } from 'react';
-import 'mind-ar/dist/mindar-image.prod.js';
-import 'aframe';
-import 'mind-ar/dist/mindar-image-aframe.prod.js';
-import './App.css';
-import MindARViewer from './mindar-viewer';
-import multiTarget from './multi-target';
-import 'mind-ar/dist/mindar-image-three.prod.js';
+import React, { useState } from "react";
+import "mind-ar/dist/mindar-image.prod.js";
+import "aframe";
+import "mind-ar/dist/mindar-image-aframe.prod.js";
+import "./App.css";
+import MindARViewer from "./mindar-viewer";
+import multiTarget from "./multi-target";
+import "mind-ar/dist/mindar-image-three.prod.js";
 
 function App() {
   const [started, setStarted] = useState(false);
+
+  // useEffect(() => {
+  //   const scanning = document.querySelector('.mindar-ui-overlay.mindar-ui-scanning')
+  //   // started ? scanning.classList.add('hidden') : scanning.classList.remove('hidden')
+  // }, [started]);
+
+  const handleStart = () => {
+    navigator.permissions
+      .query({ name: "camera" })
+      .then(permissionObj => {
+        setStarted(true);
+      })
+      .catch(error => {
+        alert("Please grant permission");
+      });
+    // checkDeniedPermissionAudio(() => setStarted(true))
+  };
+
   return (
-    <div className="App">
-      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+    <div
+      className="App"
+      style={{
+        margin: "auto",
+        // maxWidth: "60%",
+        width: "100vw",
+        height: "100vh",
+        background: `linear-gradient(
+          20deg,
+          rgba(231, 171, 23, 1) 3%,
+          rgba(211, 183, 44, 1) 60%,
+          rgba(129, 175, 118, 1) 100%
+        )`,
+        position: "relative",
+        overflow: "hidden",
+        color: "white"
+      }}>
+      <h1>Demo</h1>
 
       <div>
-	{/* {!started && <button onClick={() => {setStarted(true)}}>Start Image Tracking</button>}
+        {/* {!started && <button onClick={() => {setStarted(true)}}>Start Image Tracking</button>}
 	{started && <button onClick={() => {setStarted(false)}}>Stop Image Tracking</button>} */}
 
-  {!started && <button onClick={() => {setStarted(true)}}>Start Multi-Target</button>}
-	{started && <button onClick={() => {setStarted(false)}}>Stop Multi-Target</button>}
+        {!started && (
+          <button onClick={() => handleStart()}>Start</button>
+        )}
+        {started && (
+          <button
+            onClick={() => {
+              setStarted(false);
+            }}>
+            Stop
+          </button>
+        )}
       </div>
 
       {started && (
-	<div className="container">
-	  <MindARViewer/>
-    {/* <multiTarget/> */}
-	  <video></video>
-	</div>
-
+        <div className="container">
+          <MindARViewer />
+          {/* <multiTarget/> */}
+          {/* <video></video> */}
+        </div>
       )}
     </div>
   );
--- src/mindar-viewer.js
+++ src/mindar-viewer.js
@@ -1,44 +1,39 @@
 import React, { useEffect, useRef } from "react";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   const sceneRef = useRef(null);
+  
+  document.querySelector('.mindar-ui-scanning')?.classList.add('hidden')
 
   useEffect(() => {
     const sceneEl = sceneRef.current;
-    sceneEl.style.width = "100vh";
+    sceneEl.style.margin = "auto";
+    sceneEl.style.width = "90%";
     sceneEl.style.height = "100vh";
     const arSystem = sceneEl.systems["mindar-image-system"];
     sceneEl.addEventListener("renderstart", () => {
-      
       arSystem.start(); // start AR
     });
     return () => {
+      document.querySelectorAll('.mindar-ui-overlay').forEach(item => item.remove())
       arSystem.stop();
     };
   }, []);
 
   return (
     <>
-      {/* <button onClick={() => sceneRef.current.switchTarget(0)}>Test</button> */}
-      {/* <a-scene
+      <a-scene
         ref={sceneRef}
-        mindar-image="imageTargetSrc: ./card.mind; maxTrack: 2; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+        mindar-image="imageTargetSrc: ./computer.mind; maxTrack: 2"
         color-space="sRGB"
-        embedded
         renderer="colorManagement: true, physicallyCorrectLights"
         vr-mode-ui="enabled: false"
-        device-orientation-permission-ui="enabled: false"> */}
-
-<a-scene ref={sceneRef} mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.5/examples/image-tracking/assets/band-example/band.mind; maxTrack: 2" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
-        {/* <a-assets>
-	<img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/card.png" />
-	<a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
-      </a-assets> */}
-
+        device-orientation-permission-ui="enabled: false">
         <a-assets>
           <a-asset-item
             id="bearModel"
-            src="./assets/Ferrari-j50/scene.gltf"></a-asset-item>
+            src="./assets/Computer/scene.gltf"></a-asset-item>
           <a-asset-item
             id="raccoonModel"
             src="./assets/Coke/scene.gltf"></a-asset-item>
@@ -46,10 +41,6 @@ export default () => {
 
         <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-        {/* <a-entity mindar-image-target="targetIndex: 0">
-	<a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-	<a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
-      </a-entity> */}
         <a-entity mindar-image-target="targetIndex: 1">
           <a-gltf-model
             rotation="0 0 0 "
@@ -60,9 +51,9 @@ export default () => {
         </a-entity>
         <a-entity mindar-image-target="targetIndex: 0">
           <a-gltf-model
-            rotation="0 50 0 "
-            position="0.5 0 0"
-            scale="0.001 0.001 0.001"
+            rotation="0 0 0 "
+            position="0 -0.5 0"
+            scale="0.3 0.3 0.3"
             src="#bearModel"
             animation-mixer></a-gltf-model>
         </a-entity>
